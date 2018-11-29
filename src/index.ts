67c313e177c8b94a2dcb207205f78268cda1beb6
Ilan Garcia <ilan.f.garcia@gmail.com>
2018-11-29 11:29:41 -0300

    linting changes
    Changed order of lint and pretty
    Allow arrow functions without parenthesis

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,18 @@ const test = 1;
 setInterval(() => {
   console.log(test);
 }, 500);
+
+[].map(arr => {});
+
+function callTest(
+  param1: number,
+  param2: string,
+  param3: any,
+  param4?: boolean,
+  param5?: boolean,
+  param6?: boolean,
+) {
+  console.log('hello');
+}
+
+callTest(1, 'a', {});
